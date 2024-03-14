daf61df841fa1a3e90b952c4202a5c94475dd53f
Christian <brewer.christiang@gmail.com>
2024-03-14 13:55:35 -0400

    changes from root to app so that bootstrap will work correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,55 @@
-
+import {BrowserRouter as Router, Routes, Route, Outlet} from "react-router-dom";
 import './App.css'
+import Home from "./components/Home.jsx";
+import ErrorPage from "./components/errors/ErrorPage.jsx";
+import NavBar from "./components/NavBar.jsx";
+import {useState} from "react";
+import {getPopularMovies, getPopularMoviesByPlatform} from "./api/movies.js";
+import MoviesIndexPage from "./components/MoviesIndexPage.jsx";
+import ShowMovie from "./components/show/ShowMovie.jsx";
+import ShowTV from "./components/show/ShowTV.jsx";
+import ShowPerson from "./components/show/ShowPerson.jsx";
+import SearchResultsPage from "./components/SearchResultsPage.jsx";
+import TVIndexPage from "./components/TVIndexPage.jsx";
+import {getPopularTV} from "./api/tv.js";
+import Login from "./routes/auth/login.jsx";
+import Register from "./routes/auth/register.jsx";
 
 function App() {
 
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <Router>
+            <NavBar
+                show={show}
+                setShow={setShow}
+                handleClose={handleClose}
+                handleShow={handleShow}
+            />
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/movies" element={<MoviesIndexPage
+                    title={"Today's Hot Movies"}
+                    apiCall={getPopularMovies}/>}/>
+                <Route path="/movies/:platform" element={<MoviesIndexPage
+                    title={"Popular Movies on"}
+                    apiCall={getPopularMoviesByPlatform}/>}/>
+                <Route path="/tv/:platform" element={<TVIndexPage
+                    title={"Today's Hot Shows"}
+                    apiCall={getPopularTV}/>}/>
+                <Route path="/movie/:id" element={<ShowMovie/>}/>
+                <Route path="/tv/show/:id" element={<ShowTV/>}/>
+                <Route path="/person/:id" element={<ShowPerson/>}/>
+                <Route path="/search/multi/:term" element={<SearchResultsPage/>}/>
+                <Route path="*" element={<ErrorPage/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,98 +1,112 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App.jsx";
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Root from "./routes/root.jsx";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ErrorPage from "./components/errors/ErrorPage.jsx";
-import Register from "./routes/auth/register.jsx";
-import MoviesIndexPage from "./components/MoviesIndexPage.jsx";
-import ShowMovie from "./components/show/ShowMovie.jsx";
-import Login from "./routes/auth/login.jsx";
-import {getPopularMovies, getPopularMoviesByPlatform} from "./api/movies.js";
-import SearchResultsPage from "./components/SearchResultsPage.jsx";
-import TVIndexPage from "./components/TVIndexPage.jsx";
-import {getPopularTV, getPopularTVByPlatform} from "./api/tv.js";
-import ShowTV from "./components/show/ShowTV.jsx";
-import ShowPerson from "./components/show/ShowPerson.jsx";
-
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root/>,
-        errorElement: <ErrorPage/>,
-        children: [
-            {
-                path: "/",
-                element: <MoviesIndexPage
-                    title={"Today's Hot Movies"}
-                    apiCall={getPopularMovies}
-                />,
-            },
-            {
-                path: "/login",
-                element: <Login/>,
-            },
-            {
-                path: "/logout",
-                element: <MoviesIndexPage/>,
-            },
-            {
-                path: "/register",
-                element: <Register/>,
-            },
-            {
-                path: "/movies",
-                element: <MoviesIndexPage
-                    title={"Today's Hot Movies"}
-                    apiCall={getPopularMovies}
-                />,
-            },
-            {
-                path: "/movies/:platform",
-                element: <MoviesIndexPage
-                    title={"Popular Movies on"}
-                    apiCall={getPopularMoviesByPlatform}
-                />,
-            },
-            {
-                path: "/movie/:id",
-                element: <ShowMovie/>
-            },
-            {
-                path: "/tv",
-                element: <TVIndexPage
-                    title={"Today's Hot Shows"}
-                    apiCall={getPopularTV}
-                />,
-            },
-            {
-                path: "/tv/:platform",
-                element: <TVIndexPage
-                    title={"Popular Shows on"}
-                    apiCall={getPopularTVByPlatform}
-                />,
-            },
-            {
-                path: "/tv/show/:id",
-                element: <ShowTV/>
-            },
-            {
-                path: "/person/:id",
-                element: <ShowPerson/>
-            },
-            {
-                path: "/search/multi/:term",
-                element: <SearchResultsPage/>
-            }
-        ]
-    },
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <RouterProvider router={router}/>
-    </React.StrictMode>,
+
+        <App />
+    ,
 )
+
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import {createBrowserRouter, RouterProvider} from "react-router-dom";
+// import Root from "./routes/root.jsx";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import ErrorPage from "./components/errors/ErrorPage.jsx";
+// import Register from "./routes/auth/register.jsx";
+// import MoviesIndexPage from "./components/MoviesIndexPage.jsx";
+// import ShowMovie from "./components/show/ShowMovie.jsx";
+// import Login from "./routes/auth/login.jsx";
+// import {getPopularMovies, getPopularMoviesByPlatform} from "./api/movies.js";
+// import SearchResultsPage from "./components/SearchResultsPage.jsx";
+// import TVIndexPage from "./components/TVIndexPage.jsx";
+// import {getPopularTV, getPopularTVByPlatform} from "./api/tv.js";
+// import ShowTV from "./components/show/ShowTV.jsx";
+// import ShowPerson from "./components/show/ShowPerson.jsx";
+//
+//
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <Root/>,
+//         errorElement: <ErrorPage/>,
+//         children: [
+//             {
+//                 path: "/",
+//                 element: <MoviesIndexPage
+//                     title={"Today's Hot Movies"}
+//                     apiCall={getPopularMovies}
+//                 />,
+//             },
+//             {
+//                 path: "/login",
+//                 element: <Login/>,
+//             },
+//             {
+//                 path: "/logout",
+//                 element: <MoviesIndexPage/>,
+//             },
+//             {
+//                 path: "/register",
+//                 element: <Register/>,
+//             },
+//             {
+//                 path: "/movies",
+//                 element: <MoviesIndexPage
+//                     title={"Today's Hot Movies"}
+//                     apiCall={getPopularMovies}
+//                 />,
+//             },
+//             {
+//                 path: "/movies/:platform",
+//                 element: <MoviesIndexPage
+//                     title={"Popular Movies on"}
+//                     apiCall={getPopularMoviesByPlatform}
+//                 />,
+//             },
+//             {
+//                 path: "/movie/:id",
+//                 element: <ShowMovie/>
+//             },
+//             {
+//                 path: "/tv",
+//                 element: <TVIndexPage
+//                     title={"Today's Hot Shows"}
+//                     apiCall={getPopularTV}
+//                 />,
+//             },
+//             {
+//                 path: "/tv/:platform",
+//                 element: <TVIndexPage
+//                     title={"Popular Shows on"}
+//                     apiCall={getPopularTVByPlatform}
+//                 />,
+//             },
+//             {
+//                 path: "/tv/show/:id",
+//                 element: <ShowTV/>
+//             },
+//             {
+//                 path: "/person/:id",
+//                 element: <ShowPerson/>
+//             },
+//             {
+//                 path: "/search/multi/:term",
+//                 element: <SearchResultsPage/>
+//             }
+//         ]
+//     },
+// ])
+//
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <React.StrictMode>
+//         <RouterProvider router={router}/>
+//     </React.StrictMode>,
+// )
